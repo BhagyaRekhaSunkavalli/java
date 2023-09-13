583cb953b562fda3cdf86416ae140563a4370b0c
BhagyaRekhaSunkavalli <143427278+BhagyaRekhaSunkavalli@users.noreply.github.com>
2023-09-13 21:29:37 +0530

    Create PrimitiveTypes.java

---
+++ PrimitiveTypes.java
@@ -0,0 +1,13 @@
+import java.io.*; 
+import java.util.*; 
+class PrimitiveTypes{ 
+static byte value1; static short value2; 
+static int value3; 
+static long value4; 
+static boolean value5; 
+static double value6; 
+static float value7; 
+public static void main(String args[]){ 
+System.out.println("byte default value = "+value1); System.out.println("short default value = "+value2); System.out.println("int default value = "+value3); System.out.println("long default value = "+value4); System.out.println("boolean default value = "+value5); System.out.println("double default value = "+value6); System.out.println("float default value = "+value7); 
+} 
+} 
